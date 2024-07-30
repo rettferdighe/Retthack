c05b573bd0bd859e91034f270e60670c94f25654
rettferdighe <omerdusgun08@gmail.com>
2024-07-30 22:33:48 +0300

    donguler ve bazi seyler eklendi

--- rett.py
+++ rett.py
@@ -19,48 +19,58 @@ while True:
         "7-) Speedtest\n"
         "8-) Araba oyunu\n"
         "9-) Çeviri\n"
+        "10-) Nyancat\n"
+        "11-) Filmlerdeki hack sahnesi arayüzü\n"
         "Bir numara seçiniz: "
     )
 
     if secim == "1":
         os.system("pkg install sl -y")
         os.system("sl")
-        break
+
     elif secim == "2":
         os.system("pkg install libcaca -y")
         os.system("cacafire")
-        break
+
     elif secim == "3":
         annen = input("Nickname giriniz: ")
         os.system("pkg install figlet -y")
         os.system(f"figlet {annen}")
-        break
+        input("devam etmek icin 'Enter' tuşuna basınız")
     elif secim == "4":
         os.system("pkg install cmatrix -y")
         os.system("cmatrix")
-        break
+
     elif secim == "5":
         os.system("pkg install neofetch -y")
         os.system("neofetch")
-        break
+        input("devam etmek icin 'Enter' tusuna basiniz")
     elif secim == "6":
         baban = input("Sese çevrilecek metni giriniz: ")
         os.system("pkg install espeak -y")
         os.system(f'espeak "{baban}"')
-        break
+
     elif secim == "7":
         os.system("pkg install speedtest-go -y")
         os.system("speedtest-go")
-        break
+
     elif secim == "8":
         os.system("pkg install moon-buggy -y")
         os.system("moon-buggy")
-        break
+
     elif secim == "9":
         trans = input("çevirilecek cümleyi giriniz: ")
         os.system("pkg install translate-shell -y ")
         os.system(f'trans -b :tr "{trans}"')
-        break
+        input("devam etmek icin 'Enter' tusuna basiniz")
+    elif secim == "10":
+        os.system("pkg install nyancat")
+        os.system("nyancat")
+
+    elif secim == "11":
+        os.system("pkg install hollywood -y")
+        os.system("hollywood")
+
     else:
         print("geçersiz bir numara girdiniz. Lütfen tekrar deneyin.")
         time.sleep(2)
