b8474c90a0f405388a8b9392da30b659c45c0c51
rettferdighe <omerdusgun08@gmail.com>
2024-10-22 22:03:32 +0300

    bir kaç özellik eklendi.

---
+++ rett.py
@@ -0,0 +1,76 @@
+import os
+import time
+
+
+
+while True:
+    os.system("clear")
+    print("   ___     ___    _____   _____   _  _     ___     ___    _  __ ")
+    print("  | _ \   | __|  |_   _| |_   _| | || |   /   \   / __|  | |/ / ")
+    print("  |   /   | _|     | |     | |   | __ |   | - |  | (__   | ' <")
+    print("  |_|_\   |___|   _|_|_   _|_|_  |_||_|   |_|_|   \___|  |_|\_\ ")
+    secim = input(
+        "1-) Tren geçişi\n"
+        "2-) Şömine\n"
+        "3-) ASCII nickname\n"
+        "4-) Matrix ekranı\n"
+        "5-) Sistem bilgilerini gösterir\n"
+        "6-) Metni sese çevirme\n"
+        "7-) Speedtest\n"
+        "8-) Araba oyunu\n"
+        "9-) Çeviri\n"
+        "10-) Nyancat\n"
+        "11-) Filmlerdeki hack sahnesi arayüzü\n"
+        "Bir numara seçiniz: "
+    )
+
+    if secim == "1":
+        os.system("pkg install sl -y")
+        os.system("sl")
+
+    elif secim == "2":
+        os.system("pkg install libcaca -y")
+        os.system("cacafire")
+
+    elif secim == "3":
+        annen = input("Nickname giriniz: ")
+        os.system("pkg install figlet -y")
+        os.system(f"figlet {annen}")
+        input("devam etmek icin 'Enter' tuşuna basınız")
+    elif secim == "4":
+        os.system("pkg install cmatrix -y")
+        os.system("cmatrix")
+
+    elif secim == "5":
+        os.system("pkg install neofetch -y")
+        os.system("neofetch")
+        input("devam etmek icin 'Enter' tusuna basiniz")
+    elif secim == "6":
+        baban = input("Sese çevrilecek metni giriniz: ")
+        os.system("pkg install espeak -y")
+        os.system(f'espeak "{baban}"')
+
+    elif secim == "7":
+        os.system("pkg install speedtest-go -y")
+        os.system("speedtest-go")
+
+    elif secim == "8":
+        os.system("pkg install moon-buggy -y")
+        os.system("moon-buggy")
+
+    elif secim == "9":
+        trans = input("çevirilecek cümleyi giriniz: ")
+        os.system("pkg install translate-shell -y ")
+        os.system(f'trans -b :tr "{trans}"')
+        input("devam etmek icin 'Enter' tusuna basiniz")
+    elif secim == "10":
+        os.system("pkg install nyancat")
+        os.system("nyancat")
+
+    elif secim == "11":
+        os.system("pkg install hollywood -y")
+        os.system("hollywood")
+
+    else:
+        print("geçersiz bir numara girdiniz. Lütfen tekrar deneyin.")
+        time.sleep(2)
